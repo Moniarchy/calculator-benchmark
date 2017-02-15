5e18ddca40804105e0fe1846b1b804472397a50f
Monica Williams <monicaestellaw@gmail.com>
2017-02-15 12:43:48 -0800

    created class for calculator

---
+++ script.js
@@ -0,0 +1,51 @@
+//make sure i am talking to the DOM
+//when a button is clicked, change the number displayed
+  //write a function that changes
+
+  var Calculator = class Calculator {
+    constructor( props ) {
+
+    }
+
+    display() {
+      //display the number that was pressed
+      //if equals, percent, toggle, or clear, run immediately. 
+    }
+
+    getNumber() {
+      //by div id?
+    }
+
+    divide() {
+
+    }
+
+    multiply() {
+
+    }
+
+    subtract() {
+      return
+    }
+
+    add( addend, addend ) {
+      return addend + addend
+    }
+
+    equals() {
+
+    }
+
+    clear() {
+      //make zero
+    }
+
+    togglePositiveNegative() {
+      //append or remove negative sign from answer
+    }
+
+    makePercentage() {
+      //divide by 100 and immediately return the answer
+    }
+
+  }
