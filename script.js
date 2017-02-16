d33c852244f0094393724802c9cd146d9af26f18
Monica Williams <monicaestellaw@gmail.com>
2017-02-16 09:08:13 -0800

    got something logging to the page when I click seven

--- script.js
+++ script.js
@@ -1,51 +1,12 @@
-//make sure i am talking to the DOM
-//when a button is clicked, change the number displayed
-  //write a function that changes
+//document.querySelector( 'calculator-answer-screen' ).innerHTML = valueOfButtonClicked
 
-  var Calculator = class Calculator {
-    constructor( props ) {
+// var valueOfButtonClicked = button.innerHTML
 
-    }
+function initElement() {
+        var p = document.getElementById('calculator-seven');
+        p.onclick = showAlert;
+      }
 
-    display() {
-      //display the number that was pressed
-      //if equals, percent, toggle, or clear, run immediately. 
-    }
-
-    getNumber() {
-      //by div id?
-    }
-
-    divide() {
-
-    }
-
-    multiply() {
-
-    }
-
-    subtract() {
-      return
-    }
-
-    add( addend, addend ) {
-      return addend + addend
-    }
-
-    equals() {
-
-    }
-
-    clear() {
-      //make zero
-    }
-
-    togglePositiveNegative() {
-      //append or remove negative sign from answer
-    }
-
-    makePercentage() {
-      //divide by 100 and immediately return the answer
-    }
-
-  }
+var showAlert = function(){
+  console.log('hi')
+}
