9f8e38d640706287ba857ee87c3cdd5ab1717e26
Monica Williams <monicaestellaw@gmail.com>
2017-02-17 11:00:31 -0800

    clicking a number gets the number on the answer screen to change

--- script.js
+++ script.js
@@ -2,12 +2,7 @@ var handleEvents = function() {
   document.getElementById( 'calculator-body' )
   .addEventListener( 'click', function( e ) {
   if ( e.target && e.target.matches('div.calculator-cell')) {
-    console.log('div.calculator-answers'.innerHTML)
-    console.log(e.target.innerHTML)
+    document.querySelector( '.calculator-answers').innerHTML = e.target.innerHTML
     }
   })
 }
-
-
-//when you come back to this, you were figuring out how to get the innerHTML of the answer field
-//so you could update it
